4cf2aa032d50ae40c31b7dfa91c070f46782e224
kasumi teruya <all.splashing@gmail.com>
2021-02-01 14:57:55 +0900

    投入可能なお金の種類を定数として持つようにした

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,6 +1,8 @@
 class VendingMachine
+  INSERTABLE_MONEY = [10, 50, 100, 500, 1000]
+
   def insert(money)
-    return money unless [10, 50, 100, 500, 1000].include?(money)
+    return money unless INSERTABLE_MONEY.include?(money)
 
     0
   end
