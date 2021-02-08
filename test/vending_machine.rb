032042b1d5d1f03855f45739bdf677ec8a2b4c60
kasumi teruya <all.splashing@gmail.com>
2021-02-08 15:27:26 +0900

    テスト を spec で書くようにした

--- test/vending_machine.rb
+++ test/vending_machine.rb
@@ -1,82 +1,107 @@
 require 'minitest/autorun'
 require './vending_machine'
 
-class TestVendingMachine < Minitest::Test
-  def setup
+describe VendingMachine do
+  before do
     @vending_machine = VendingMachine.new
   end
 
-  def test_insert_10_yen_bill
-    assert_equal 0, @vending_machine.insert(10)
+  describe '#insert' do
+    it '10円 を insert すると、0円 が返ること' do
+      _(@vending_machine.insert(10)).must_equal 0
+    end
+
+    it '50円 を insert すると、0円 が返ること' do
+      _(@vending_machine.insert(50)).must_equal 0
+    end
+
+    it '100円 を insert すると、0円 が返ること' do
+      _(@vending_machine.insert(100)).must_equal 0
+    end
+
+    it '500円 を insert すると、0円 が返ること' do
+      _(@vending_machine.insert(500)).must_equal 0
+    end
+
+    it '1000円 を insert すると、0円 が返ること' do
+      _(@vending_machine.insert(1000)).must_equal 0
+    end
+
+    it '5000円 を insert すると、5000円 が返ること' do
+      _(@vending_machine.insert(5000)).must_equal 5000
+    end
+
+    it '想定外のものを insert すると、insert したものが返ること' do
+      _(@vending_machine.insert('葉っぱのおかね')).must_equal '葉っぱのおかね'
+    end
   end
 
-  def test_insert_50_yen_coin
-    assert_equal 0, @vending_machine.insert(50)
+  describe '#input_amount' do
+    it 'insert した金額が返ること' do
+      @vending_machine.insert(1000)
+      _(@vending_machine.input_amount).must_equal 1000
+    end
+
+    it '複数回 insert したとき、その合計金額が返ること' do
+      @vending_machine.insert(100)
+      @vending_machine.insert(1000)
+      _(@vending_machine.input_amount).must_equal 1100
+    end
   end
 
-  def test_insert_100_yen_coin
-    assert_equal 0, @vending_machine.insert(100)
+  describe '#refund' do
+    it 'insert した合計金額が返ること' do
+      @vending_machine.insert(100)
+      @vending_machine.insert(1000)
+      _(@vending_machine.refund).must_equal 1100
+    end
+
+    it 'input_amount が 0 になること' do
+      @vending_machine.insert(100)
+      @vending_machine.insert(1000)
+      @vending_machine.refund
+      _(@vending_machine.input_amount).must_equal 0
+    end
   end
 
-  def test_insert_500_yen_coin
-    assert_equal 0, @vending_machine.insert(500)
+  describe '#stock_tally' do
+    it '初期状態として 120 円のコーラが 5 本入っており、その情報が返ること' do
+      _(@vending_machine.stock_tally).must_equal({ 'コーラ' => { price: 120, count: 5 } })
+    end
   end
 
-  def test_insert_1000_yen_coin
-    assert_equal 0, @vending_machine.insert(1000)
+  describe '#test_store' do
+    it 'ドリンクを投入できること' do
+      _(@vending_machine.store(Cola.new).size).must_equal(6)
+    end
+
+    it '投入したドリンクの情報が在庫情報に反映されること' do
+      @vending_machine.store(Cola.new)
+      _(@vending_machine.stock_tally).must_equal({ 'コーラ' => { price: 120, count: 6 } })
+    end
   end
 
-  def test_insert_uninsertable_money
-    assert_equal 5000, @vending_machine.insert(5000)
+  describe '#buy?' do
+    it '投入金額が足りている場合、 true が返ること' do
+      @vending_machine.insert(500)
+      _(@vending_machine.buy?(:cola)).must_equal(true)
+    end
+
+    it '投入金額が足りていない場合、 false が返ること' do
+      @vending_machine.insert(100)
+      _(@vending_machine.buy?(:cola)).must_equal(false)
+    end
   end
 
-  def test_insert_unknown_object
-    assert_equal '葉っぱのおかね', @vending_machine.insert('葉っぱのおかね')
-  end
+  describe '#buy' do
+    it '投入金額が足りている場合、 購入したドリンクが返ること' do
+      @vending_machine.insert(500)
+      _(@vending_machine.buy(:cola)).must_be_instance_of(Cola)
+    end
 
-  def test_input_amount
-    @vending_machine.insert(1000)
-    assert_equal 1000, @vending_machine.input_amount
-  end
-
-  def test_insert_money_2_times
-    @vending_machine.insert(100)
-    @vending_machine.insert(1000)
-    assert_equal 1100, @vending_machine.input_amount
-  end
-
-  def test_refund
-    @vending_machine.insert(100)
-    @vending_machine.insert(1000)
-    assert_equal 1100, @vending_machine.refund
-    assert_equal 0, @vending_machine.input_amount
-  end
-
-  def test_stock_tally
-    assert_equal({ 'コーラ' => { price: 120, count: 5 } }, @vending_machine.stock_tally)
-  end
-
-  def test_store
-    @vending_machine.store(Cola.new)
-    assert_equal({ 'コーラ' => { price: 120, count: 6 } }, @vending_machine.stock_tally)
-  end
-
-  def test_buy_if_enough_input_amount
-    @vending_machine.insert(500)
-    assert_equal(true, @vending_machine.buy?(:cola))
-  end
-
-  def test_buy_if_not_enough_input_amount
-    assert_equal(false, @vending_machine.buy?(:cola))
-  end
-
-  def test_buy_drink_if_can_buy
-    @vending_machine.insert(500)
-    assert_equal(Cola, @vending_machine.buy(:cola).class)
-  end
-
-  def test_buy_drink_if_cannot_buy
-    @vending_machine.insert(100)
-    assert_nil(@vending_machine.buy(:cola))
+    it '投入金額が足りていない場合、 nil が返ること' do
+      @vending_machine.insert(100)
+      _(@vending_machine.buy(:cola)).must_be_nil
+    end
   end
 end
